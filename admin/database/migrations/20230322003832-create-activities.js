9adaa12aff5d29c95cf6726bdd2520441c77e7e9
Dayanna Vanessa Suarez M <dayanna.suarez@uao.edu.co>
2023-03-26 23:52:51 -0500

    Añadir actividades a migration

--- admin/database/migrations/20230322003832-create-activities.js
+++ admin/database/migrations/20230322003832-create-activities.js
@@ -1,11 +1,36 @@
 'use strict';
 
-const { ACTIVITY_TABLE, ActivitySchema } = require('../models/activities.model');
+const {
+  ACTIVITY_TABLE,
+  ActivitySchema,
+} = require('../models/activities.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
     await queryInterface.createTable(ACTIVITY_TABLE, ActivitySchema);
+    await queryInterface.bulkInsert(ACTIVITY_TABLE, [
+      {
+        description: 'Inicio jornada',
+        type: 'START',
+        isFreeTime: false,
+      },
+      {
+        description: 'Fin jornada',
+        type: 'FINISH',
+        isFreeTime: false,
+      },
+      {
+        description: 'Inicio descanso',
+        type: 'START',
+        isFreeTime: true,
+      },
+      {
+        description: 'Fin descanso',
+        type: 'FINISH',
+        isFreeTime: true,
+      },
+    ]);
   },
 
   async down(queryInterface) {
